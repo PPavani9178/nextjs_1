8fda2ebad4e82cf5060512d6935c97d2fbf2ac95
Peketi <pavani1642002@gmail.com>
2024-06-30 15:18:16 +0530

    added

--- next2/my-nextjs-app/src/app/pages/about.js
+++ next2/my-nextjs-app/src/app/pages/about.js
@@ -1,9 +1,16 @@
+// export default function About() {
+//   return (
+//     <div>
+//       <h1>About Page</h1>
+//       <p>This is the About page.</p>
+//     </div>
+//   );
+// }
 export default function About() {
     return (
-      <div>
-        <h1>About Page</h1>
+      <div style={{ backgroundColor: '#fff', padding: '20px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+        <h1 style={{ fontSize: '2rem', color: '#333', marginBottom: '20px' }}>About Page</h1>
         <p>This is the About page.</p>
       </div>
     );
   }
-  
--- next2/my-nextjs-app/src/app/pages/index.js
+++ next2/my-nextjs-app/src/app/pages/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
+// import React from 'react';
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//       <p>Welcome to the Home page!</p>
+//     </div>
+//   );
+// }
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Home Page</h1>
       <p>Welcome to the Home page!</p>
     </div>
   );
